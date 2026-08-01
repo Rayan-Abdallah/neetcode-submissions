d22fa37c283f2955d9759041cd507095cba73b07
Rayan Abdallah <rayanabdallahofficial@gmail.com>
2026-08-01 14:08:19 +0100

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<pair<int, int> > freqToVal;
+        int arr[2005] = {0};
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] < 0){
+                arr[1000 + (nums[i]*-1)]++;
+            }
+            else{
+                arr[nums[i]]++;
+            }
+        }
+        for(int i = 0; i<=1000; i++){
+            freqToVal.push_back({arr[i], i});
+        }
+        for(int i = 1001; i <= 2000; i++){
+            freqToVal.push_back({arr[i], (i - 1000)*-1});
+        }
+        sort(freqToVal.begin(), freqToVal.end());
+        vector<int> ans;
+        for(int i = freqToVal.size() - k; i < freqToVal.size(); i++){
+            ans.push_back(freqToVal[i].second);
+        }
+        return ans;
+    }
+};
