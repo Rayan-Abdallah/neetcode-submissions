505ef2c97f6a5219f7ac7786532c89e6d38bcc5b
Rayan Abdallah <rayanabdallahofficial@gmail.com>
2026-08-19 14:26:05 +0100

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.cpp
@@ -0,0 +1,62 @@
+class PrefixTree {
+private:
+    struct Node {
+        char c;
+        vector<Node*> children;
+        Node(char x): c(x){
+
+        }
+        
+        Node* add(char x){
+            bool found = false;
+            for(auto node: children){
+                if(node->c == x){
+                    return node;
+                }
+            }
+            Node* child = new Node(x);
+            children.push_back(child);
+            return child;
+        }
+    };
+    Node start = Node('0');
+public:
+    PrefixTree() {
+        
+    }
+    
+    void insert(string word) {
+        Node* cur = &start;
+       for(auto c: word){
+            Node* next = cur->add(c);
+            cur = next;
+       }
+       cur->add('0');
+    }
+    
+    bool search(string word) {
+        string s = word;
+        s += '0';
+        return startsWith(s);
+    }
+    
+    bool startsWith(string prefix) {
+        Node* cur = &start;
+        bool failed = false;
+        for(auto c: prefix){
+            bool found = false;
+            for(auto node: cur->children){
+                if(node->c == c){
+                    found = true;
+                    cur = node;
+                    break;
+                }
+            }
+            if(!found){
+                failed = true;
+                break;
+            }
+        }
+        return !failed;
+    }
+};
