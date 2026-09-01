37d6df26e65ae3a311e01684d9c966dca6b6e277
Rayan Abdallah <rayanabdallahofficial@gmail.com>
2026-09-01 13:07:18 +0100

    Add: design-twitter-feed - submission-1

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-1.cpp
@@ -0,0 +1,32 @@
+class Twitter {
+private:
+unordered_map<int, unordered_set<int> > adj;
+vector<pair<int, int> > posts;
+public:
+    Twitter() {
+        
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        posts.push_back({userId, tweetId});
+        
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        vector<int> res;
+        for(int i = posts.size() - 1; i >= 0 && res.size() < 10; i--){
+            if(posts[i].first == userId || adj[userId].find(posts[i].first) != adj[userId].end()){
+                res.push_back(posts[i].second);
+            }
+        }
+        return res;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        adj[followerId].insert(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        adj[followerId].erase(followeeId);
+    }
+};
