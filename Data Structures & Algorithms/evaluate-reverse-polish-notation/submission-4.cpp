a3c915f9bf7b28151597e6724b23c7fd13d46393
Rayan Abdallah <rayanabdallahofficial@gmail.com>
2026-09-03 09:56:20 +0100

    Add: evaluate-reverse-polish-notation - submission-4

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-4.cpp
@@ -0,0 +1,52 @@
+#include <string>
+
+class Solution {
+public:
+    int stoi(string& s){
+        bool neg = false;
+        int i = 0;
+        if(s[0] == '-'){
+            neg = true;
+            i++;
+        }
+        int res = 0;
+        for(i; i < s.size(); i++){
+            res *= 10;
+            res += (s[i] - '0');
+        }
+        if(neg){
+            res *= -1;
+        }
+        return res;
+    }
+
+    int evalRPN(vector<string>& tokens) {
+        bool startOp = true, start = true;
+        deque<int> dq;
+        for(int i = 0; i < tokens.size(); i++){
+            if(tokens[i].size() == 1 && (tokens[i][0] < '0' || tokens[i][0] > '9')){
+                int r = dq.back();
+                dq.pop_back();
+                int l = dq.back();
+                dq.pop_back();
+                if(tokens[i][0] == '+'){
+                    l += r;
+                }
+                else if(tokens[i][0] == '*'){
+                    l *= r;
+                }
+                else if(tokens[i][0] == '/'){
+                    l /= r;
+                }
+                else{
+                    l -= r;
+                }
+                dq.push_back(l);
+            }
+            else{
+                dq.push_back(stoi(tokens[i]));
+            }
+        }
+        return dq.front();
+    }
+};
