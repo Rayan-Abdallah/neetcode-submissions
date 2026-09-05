1e1c0ca906452ca0e8b03ffbb947ea8a13e605db
Rayan Abdallah <rayanabdallahofficial@gmail.com>
2026-09-05 21:25:02 +0100

    Add: jump-game-ii - submission-4

---
+++ Data Structures & Algorithms/jump-game-ii/submission-4.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        queue<pair<int, int>> pq;
+        pq.push({0, 0});
+        int ans = 1001;
+        bool vis[1005] = {0};
+        vis[0] = 1;
+        while(!pq.empty()){
+            pair<int, int> p = pq.front();
+            int ind = p.second, jump = p.first;
+            pq.pop();
+            if(ind == nums.size() - 1){
+                ans = min(ans, jump);
+                continue;
+            }
+            for(int i = ind + 1; i <= min(ind + nums[ind], (int)nums.size()-1); i++){
+                if(!vis[i]){
+                    pq.push({jump + 1, i});
+                    vis[i] = 1;
+                }
+            }
+        }
+        return ans;
+    }
+};
