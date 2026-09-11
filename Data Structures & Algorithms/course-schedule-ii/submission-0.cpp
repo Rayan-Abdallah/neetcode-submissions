93c68f6ba3cf97d5523367c30e75268cbe1bacdb
Rayan Abdallah <rayanabdallahofficial@gmail.com>
2026-09-11 14:07:01 +0100

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> inDeg(numCourses, 0);
+        vector<vector<int>> adj(numCourses, vector<int>());
+        for(auto p: prerequisites){
+            inDeg[p[0]]++;
+            adj[p[1]].push_back(p[0]);
+        }
+        queue<int> q;
+        vector<int> ans;
+        for(int i = 0; i < numCourses; i++){
+            if(!inDeg[i]){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int ind = q.front();
+            q.pop();
+            ans.push_back(ind);
+            for(auto v: adj[ind]){
+                inDeg[v]--;
+                if(!inDeg[v]){
+                    q.push(v);
+                }
+            }
+        }
+        if(ans.size() != numCourses){
+            return {};
+        }
+        return ans;
+    }
+};
