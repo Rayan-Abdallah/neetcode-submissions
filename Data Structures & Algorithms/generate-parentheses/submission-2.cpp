417f04902a85904ebb2fe91665713b0734b24f74
Rayan Abdallah <rayanabdallahofficial@gmail.com>
2026-09-23 14:21:58 +0100

    Add: generate-parentheses - submission-2

---
+++ Data Structures & Algorithms/generate-parentheses/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    vector<string> ans;
+    void rec(string& cur, int op, int n){
+        if((op < 0) || (op > (n - cur.size()))){
+            return;
+        }
+        if(cur.size() == n){
+            if(op){
+                return;
+            }
+            ans.push_back(cur);
+            return;
+        }
+        cur += '(';
+        rec(cur, op + 1, n);
+        cur.pop_back();
+        cur += ')';
+        rec(cur, op - 1, n);
+        cur.pop_back();
+    }
+public:
+    vector<string> generateParenthesis(int n) {
+        string cur = "";
+        rec(cur, 0, n * 2);
+        return ans;
+    }
+};
